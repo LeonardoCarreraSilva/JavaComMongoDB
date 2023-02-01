d42b9f8366accde5155f7643d3ca4716b7237f09
Leonardo Carrera da Silva <59487403+LeonardoCarreraSilva@users.noreply.github.com>
2023-02-01 06:19:22 -0300

    Refatoracao de metodos de inserte e json com dados para o banco de dados

--- src/main/java/br/com/leonardo/CRUDMongo.java
+++ src/main/java/br/com/leonardo/CRUDMongo.java
@@ -2,6 +2,7 @@ package br.com.leonardo;
 
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import org.bson.Document;
 
@@ -19,39 +20,45 @@ public class CRUDMongo {
 		Document first = alunos.find().first();
 		return first.toString();
 	}
-	
+
 	public String doFindByName(String nome) {
 		String retorno;
 		try {
 			Document first = alunos.find(Filters.eq("nome", nome)).first();
 			retorno = first.toString();
-		}catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println("Realmente deletou o caboclo");
 			retorno = "";
 		}
 		return retorno;
 	}
-	
-	
+
 	public String doInsert(String nome) {
+		Date dataNascimento = new Date(2003, 10, 10);
+		Document curso = new Document("nome", "Historia");
+		List<Integer> notas = Arrays.asList(10, 9, 8);
+		List<Document> habilidades = Arrays.asList(new Document("nome", "Inglês")
+														.append("nível", "Básico"),
+				new Document("nome", "Espanhol")
+				.append("nível", "Básico"));
+
 		Document novoAluno = new Document("nome", nome)
-				  .append("data_nascimento", new Date(2003, 10, 10))
-				  .append("curso", new Document("nome", "Historia"))
-				  .append("notas", Arrays.asList(10, 9, 8))
-				  .append("habilidades", Arrays.asList(
-				          new Document().append("nome", "Ingles").append("nível", "Básico"),
-				          new Document().append("nome", "Espanhol").append("nível", "Básico")));
-		
+				.append("data_nascimento", dataNascimento)
+				.append("curso", curso)
+				.append("notas", notas)
+				.append("habilidades", habilidades);
+
 		alunos.insertOne(novoAluno);
 		Document first = alunos.find(Filters.eq("nome", nome)).first();
 		return first.toString().stripIndent();
+
 	}
-	
+
 	public String doUpdate(String nome, String novoNome) {
 		alunos.updateOne(Filters.eq("nome", nome), new Document("$set", new Document("nome", novoNome)));
 		return alunos.find(Filters.eq("nome", novoNome)).first().toJson();
 	}
-	
+
 	public void doDelete(String nome) {
 		alunos.deleteOne(Filters.eq("nome", nome));
 	}
