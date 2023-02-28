6e21308993aef6b6caa29ed1c109c1fcbe9b049b
Leonardo Carrera da Silva <59487403+LeonardoCarreraSilva@users.noreply.github.com>
2023-02-28 08:46:05 -0300

    Commit inicial.

---
+++ src/main/java/br/com/leonardo/escolaleo/EscolaLeoApplication.java
@@ -0,0 +1,13 @@
+package br.com.leonardo.escolaleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaLeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaLeoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/leonardo/escolaleo/controller/AdminController.java
@@ -0,0 +1,12 @@
+package br.com.leonardo.escolaleo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
